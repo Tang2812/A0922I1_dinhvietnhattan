d49fc8e23c6070edb93a50ddd7a6e9572a4897f4
Tang2812 <dinhvietnhattan@gmail.com>
2023-11-23 21:40:50 +0700

    update project

---
+++ M3/project.sql
@@ -0,0 +1,1 @@
+SELECT * FROM project.new_table;
---
+++ M6/coding/coding_6912/src/test/java/com/example/coding_6912/Coding6912ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coding_6912;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coding6912ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
